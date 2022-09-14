30f7d14f4b5ba6d14b13944e6fc35937cb9df3de
Kas Eam <KassandraEam@gmail.com>
2022-09-13 22:58:32 -0500

    Completed Notes

--- server/template_server.js
+++ server/template_server.js
@@ -8,7 +8,10 @@ const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
 
 // Route includes
+// TODO: STEP 4 - Now go to the Routes folder, and open template.router.js.
+const routeYouWillGoTo = require('./routes/template.router'); 
 const userRouter = require('./routes/user.router');
+
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -22,6 +25,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 /* Routes */
+// TODO: STEP 3 - This file path here needs to match your axios request in your SAGA. When you make your GET request, it comes here and looks for the path that you've provided in the GET. Once it finds it, it runs the Route. In this case, it goes to line 12.
+app.use('/This_Should_Match_Between_The_Saga_And_Server', routeYouWillGoTo)
 app.use('/api/user', userRouter);
 
 // Serve static files
